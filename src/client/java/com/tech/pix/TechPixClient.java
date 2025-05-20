1d6ef8d6b767cd36d68184384e3913dde45a57a6
Jean Ap. P. G. <jeangasoto@gmail.com>
2025-05-20 18:10:49 -0300

    O começo dos tempos

---
+++ src/client/java/com/tech/pix/TechPixClient.java
@@ -0,0 +1,10 @@
+package com.tech.pix;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TechPixClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
