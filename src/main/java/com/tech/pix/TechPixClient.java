1bf54342f282867d5b48d5797770af30b01a0d3c
Jean Ap. P. G. <jeangasoto@gmail.com>
2025-05-25 11:36:21 -0300

    Configurações iniciais

---
+++ src/main/java/com/tech/pix/TechPixClient.java
@@ -0,0 +1,11 @@
+package com.tech.pix;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TechPixClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
